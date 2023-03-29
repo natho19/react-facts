8cda33c019348c07826600d37d2f03e02d94310f
Nathan Akpawu <nakpawu@gmail.com>
2023-03-29 20:11:53 +0000

    header integration

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ function Header() {
             <nav className="nav">
                 <img className="nav--logo" src={Logo} alt="logo" />
                 <h3 className="nav--title">ReactFacts</h3>
-                <h4 className="nav-project">React Course - Project 1</h4>
+                <h4 className="nav--project">React Course - Project 1</h4>
             </nav>
         </header>
     );
