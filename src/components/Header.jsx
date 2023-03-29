9cae9bab0dcebafc9c8ea61de5720993cf9ddabd
Nathan Akpawu <nakpawu@gmail.com>
2023-03-29 19:57:13 +0000

    semantic html

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,15 @@
+import Logo from '../assets/logo.svg';
+
 function Header() {
-    return <div>Le Header</div>;
+    return (
+        <header>
+            <nav className="nav">
+                <img className="nav--logo" src={Logo} alt="logo" />
+                <h3 className="nav--title">ReactFacts</h3>
+                <h4 className="nav-project">React Course - Project 1</h4>
+            </nav>
+        </header>
+    );
 }
 
 export default Header;
